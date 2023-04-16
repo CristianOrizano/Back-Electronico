4c2e24cafa7e87215d3e84f6b878b5cdc6454b97
CristianOrizano <cristianorizano57@gmail.com>
2023-04-16 00:15:56 -0500

    ruta config

--- src/main/java/com/ApiRest/controller/CiudadController.java
+++ src/main/java/com/ApiRest/controller/CiudadController.java
@@ -32,7 +32,7 @@ import com.ApiRest.service.CiudadService;
 
 @RestController
 @RequestMapping("/rest")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://cyberelectronic.netlify.app")
 public class CiudadController {
 	
 	
